3b6f1c453f468dc4dbe55b4289f9a5af69164503
Victor Boateng <boatengv@Victors-MacBook-Air.local>
2024-01-06 12:06:19 +0000

    Adding a Home page with the Readlist table

---
+++ app/Home/page.tsx
@@ -0,0 +1,132 @@
+
+
+const Home = () => {
+
+    return (  
+        
+        <div className="grid grid-cols-3 gap-4 mt-10">
+            <div className="border-2 border-black ml-4">
+                <h1 className="border-2 border-b-black text-center font-serif text-3xl py-4">START</h1> 
+                <div className="grid grid-cols-2 gap-2 px-2 pt-4">                
+                    <div className="border-2 border-black">  
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 float-end cursor-e-resize">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+                        </svg>   
+                        <div className="h-64">
+                            <img src="https://static.bunnycdn.ru/i/cache/images/0/02/02dd45b5aa9d4ca36a2e0804dffaac94.jpg" className="object-cover h-full w-full"></img>
+                        </div>
+                        <h1 className="text-center">Book Title</h1>
+                        <h1 className="text-center">Book Subtitle</h1>
+                        <h1 className="text-center">Book Author</h1>
+                        <h1 className="text-center">Book Published Date</h1>  
+                    </div>
+                    <div className="">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 float-end cursor-e-resize">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+                            </svg>
+                        </div> 
+                        <div className="border border-black h-64">
+                            <img src="https://static.bunnycdn.ru/i/cache/images/0/02/02dd45b5aa9d4ca36a2e0804dffaac94.jpg" className="object-cover h-full w-full"></img>
+                        </div>
+                        <h1 className="text-center">Book Title</h1>
+                        <h1 className="text-center">Book Subtitle</h1>
+                        <h1 className="text-center">Book Author</h1>
+                        <h1 className="text-center">Book Published Date</h1>  
+                    </div>
+                    <div className="">
+                        <div className="border border-black h-64">
+                            <img src="https://static.bunnycdn.ru/i/cache/images/0/02/02dd45b5aa9d4ca36a2e0804dffaac94.jpg" className="object-cover h-full w-full"></img>
+                        </div>
+                        <h1 className="text-center">Book Title</h1>
+                        <h1 className="text-center">Book Subtitle</h1>
+                        <h1 className="text-center">Book Author</h1>
+                        <h1 className="text-center">Book Published Date</h1>  
+                    </div>
+                    <div className="">
+                        <div className="border border-black h-64">
+                            <img src="https://static.bunnycdn.ru/i/cache/images/0/02/02dd45b5aa9d4ca36a2e0804dffaac94.jpg" className="object-cover h-full w-full"></img>
+                        </div>
+                        <h1 className="text-center">Book Title</h1>
+                        <h1 className="text-center">Book Subtitle</h1>
+                        <h1 className="text-center">Book Author</h1>
+                        <h1 className="text-center">Book Published Date</h1>  
+                    </div>
+                    <div className="">
+                        <div className="border border-black h-64">
+                            <img src="https://static.bunnycdn.ru/i/cache/images/0/02/02dd45b5aa9d4ca36a2e0804dffaac94.jpg" className="object-cover h-full w-full"></img>
+                        </div>
+                        <h1 className="text-center">Book Title</h1>
+                        <h1 className="text-center">Book Subtitle</h1>
+                        <h1 className="text-center">Book Author</h1>
+                        <h1 className="text-center">Book Published Date</h1>  
+                    </div>
+                    <div className="">
+                        <div className="border border-black h-64">
+                            <img src="https://static.bunnycdn.ru/i/cache/images/0/02/02dd45b5aa9d4ca36a2e0804dffaac94.jpg" className="object-cover h-full w-full"></img>
+                        </div>
+                        <h1 className="text-center">Book Title</h1>
+                        <h1 className="text-center">Book Subtitle</h1>
+                        <h1 className="text-center">Book Author</h1>
+                        <h1 className="text-center">Book Published Date</h1>  
+                    </div>
+                    <div className="">
+                        <div className="border border-black h-64">
+                            <img src="https://static.bunnycdn.ru/i/cache/images/0/02/02dd45b5aa9d4ca36a2e0804dffaac94.jpg" className="object-cover h-full w-full"></img>
+                        </div>
+                        <h1 className="text-center">Book Title</h1>
+                        <h1 className="text-center">Book Subtitle</h1>
+                        <h1 className="text-center">Book Author</h1>
+                        <h1 className="text-center">Book Published Date</h1>  
+                    </div>
+                    <div className="">
+                        <div className="border border-black h-64">
+                            <img src="https://static.bunnycdn.ru/i/cache/images/0/02/02dd45b5aa9d4ca36a2e0804dffaac94.jpg" className="object-cover h-full w-full"></img>
+                        </div>
+                        <h1 className="text-center">Book Title</h1>
+                        <h1 className="text-center">Book Subtitle</h1>
+                        <h1 className="text-center">Book Author</h1>
+                        <h1 className="text-center">Book Published Date</h1>  
+                    </div>
+                    <div className="">
+                        <div className="border border-black h-64">
+                            <img src="https://static.bunnycdn.ru/i/cache/images/0/02/02dd45b5aa9d4ca36a2e0804dffaac94.jpg" className="object-cover h-full w-full"></img>
+                        </div>
+                        <h1 className="text-center">Book Title</h1>
+                        <h1 className="text-center">Book Subtitle</h1>
+                        <h1 className="text-center">Book Author</h1>
+                        <h1 className="text-center">Book Published Date</h1>  
+                    </div>
+                
+                </div>
+            </div>
+         
+            <div className="border border-black">
+                <h1>PROGRESS</h1>
+                <div className="grid grid-cols-2 gap-2 border border-black">                
+                    <div className="">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 float-start cursor-w-resize">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 float-end cursor-e-resize">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+                        </svg>
+                        <div className="border border-black h-64">
+                            <img src="https://static.bunnycdn.ru/i/cache/images/0/02/02dd45b5aa9d4ca36a2e0804dffaac94.jpg" className="object-cover h-full w-full"></img>
+                        </div>
+                        <h1 className="text-center">Book Title</h1>
+                        <h1 className="text-center">Book Subtitle</h1>
+                        <h1 className="text-center">Book Author</h1>
+                        <h1 className="text-center">Book Published Date</h1>  
+                    </div>
+                </div>
+            </div>
+            <div className="border border-black mr-4">
+                <h1>DONE</h1>
+            </div>
+        </div>   
+      
+    )
+}
+
+export default Home
